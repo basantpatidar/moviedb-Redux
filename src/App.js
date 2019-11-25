4cef7fee6cd03264947ce47a696806f14ca5d8d8
basantpatidar33@gmail.com <basantpatidar33@gmail.com>
2019-11-24 21:31:52 -0500

    middlewares

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import SearchBar from './SearchBar.js'
+import SearchBar from './SearchBar.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <SearchBar/>
+        <SearchBar />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,14 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {createStore} from "redux"
-import {Provider} from 'react-redux'
-import searchReducer from './reducers'; 
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import searchReducer from './reducers';
+import thunk from 'redux-thunk';
+import { createLogger } from 'redux-logger';
 
-const store = createStore(searchReducer);
+const middlewares = [];
+middlewares.push(thunk);
+
+const loggerMiddleware = createLogger({
+  predicate: () => process.env.NODE_ENV === 'development'
+});
+middlewares.push(loggerMiddleware);
+// middlewares.push(window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+const store = createStore(
+  searchReducer,
+  compose(applyMiddleware(...middlewares))
+);
 
 ReactDOM.render(
   <Provider store={store}>
-  <App />
-  </Provider>
-  , document.getElementById('root'));
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
