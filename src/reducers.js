187f02c64c96281cffe29728c08a6f4ac88cddcd
basantpatidar33@gmail.com <basantpatidar33@gmail.com>
2019-12-14 22:05:08 -0500

    update

--- src/reducers.js
+++ src/reducers.js
@@ -2,17 +2,7 @@ import { combineReducers } from 'redux';
 
 const initialState = {
   searchTerm: '',
-<<<<<<< HEAD
-  movies: []
-=======
-  name: '',
- year: '
->>>>>>> b19513d49b09f88cf75cd3307236f75653b89f9c
 };
-
-const searchReducer = (state = initialState, action) => {
-  if (action.type === 'SEARCH_TERM_DISPATCH') {
-    return { ...state, searchTerm: action.payload };
     // {  movies: [], serachTerm: action.payload}
   }
   if (action.type === 'SET_MOVIES') {
