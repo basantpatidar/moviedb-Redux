812fd0fdd56831f372997d1849088220fa758e86
basantpatidar33@gmail.com <basantpatidar33@gmail.com>
2020-01-26 22:43:02 -0500

    fix

--- src/actions.js
+++ src/actions.js
@@ -14,15 +14,15 @@ export const searchTermDispatch = term => async dispatch => {
   });
 };
 
-export const setMovies = () => async (dispatch, getState) => {
-  const {
-    searchReducer: { searchTerm }
-  } = getState();
-  const res = await axios.get(
-    `http://www.omdbapi.com/?apiKey=4b16516651637&s=${searchTerm}`
-  );
-  dispatch({
-    type: 'SET_MOVIES',
-    payload: res.data.Search
-  });
-};
+// export const setMovies = () => async (dispatch, getState) => {
+//   const {
+//     searchReducer: { searchTerm }
+//   } = getState();
+//   const res = await axios.get(
+//     `http://www.omdbapi.com/?apiKey=4b16516651637&s=${searchTerm}`
+//   );
+//   dispatch({
+//     type: 'SET_MOVIES',
+//     payload: res.data.Search
+//   });
+// };
